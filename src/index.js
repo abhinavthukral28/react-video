8b98c81448ad7e5f2c9c76f540f4516ca6a4a75d
Abhinav Thukral <abhinav@abhinavthukral.com>
2018-03-06 15:33:58 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Create a new component
+const App = () => {
+  return <div>Hi</div>
+}
+
+ReactDOM.render(<App></App>, document.querySelector('.container'));
+// Take this component's generated html and put it on the page
