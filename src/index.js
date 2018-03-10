d1bfc5771748e499e1b4b499e1daadf71de5edbb
Abhinav Thukral <abhinav@abhinavthukral.com>
2018-03-10 13:46:49 +0530

    Youtube API added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 // Create a new component
-const App = () => {
-  return <div>Hi</div>
+const API_KEY = 'AIzaSyBKKlOZXryegsW0arpTKdMswPIDpP--ikY';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'epic music'
+    }, (videos) => {
+      this.setState({videos});
+    })
+  }
+  render() {
+    return (<div>
+      <SearchBar></SearchBar>
+      <VideoList videos={this.state.videos}></VideoList>
+    </div>);
+
+  }
 }
 
 ReactDOM.render(<App></App>, document.querySelector('.container'));
