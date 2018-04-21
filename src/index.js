54bcc34b2874b27331df80cf4b30d9a1083a330a
Abhinav Thukral <abhinav@abhinavthukral.com>
2018-04-21 17:20:42 +0530

    Throttled Search Working

--- src/index.js
+++ src/index.js
@@ -4,36 +4,43 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/vedio_detail';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 // Create a new component
-const API_KEY = 'AIzaSyBKKlOZXryegsW0arpTKdMswPIDpP--ikY';
+const API_KEY = 'AIzaSyCUI_N3lBE_vTmFi9Fu6BKA6Y140dpfz8I';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-        videos: [],
-        selectedVideo: null
-    };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch("Epic Music");
+    }
 
-    YTSearch({
-      key: API_KEY,
-      term: 'epic music'
-    }, (videos) => {
-      this.setState({
-          videos: videos,
-          selectedVideo: videos[0]
-      });
-    })
-  }
-  render() {
-    return (<div>
-      <SearchBar></SearchBar>
-        <VideoDetail video={this.state.selectedVideo}></VideoDetail>
-      <VideoList videos={this.state.videos}></VideoList>
-    </div>);
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        })
+    }
 
-  }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 1000);
+        return (<div>
+            <SearchBar onSearchTermChange={term => videoSearch(term)}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos}
+            />
+        </div>);
+
+    }
 }
 
 ReactDOM.render(<App></App>, document.querySelector('.container'));
